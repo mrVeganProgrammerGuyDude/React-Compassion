b30953083bd3b29c9b5b218e54a9ef439c531ee4
Agent Broccoli <hellosheldonstewart@gmail.com>
2020-11-20 17:44:27 -0500

    sc

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = '6da5bdad';
+  const APP_KEY = '97bfacae806c312c74be93803d963c42';
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState(''); // This updates every time the user puts any letters in
+  const [query, setQuery] = useState('chicken'); //this is designed to only run when the submit button is pressed 
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]) // this will only run when "query" is updated
+
+
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits)
+    console.log(data.hits);
+  }
+
+const updateSearch = e => {
+  setSearch(e.target.value);
+  console.log(search);
+}
+
+const getSearch = e => {
+  e.preventDefault(); //This stops the automatic page refresh
+  setQuery(search);
+  setSearch('');
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form"> 
+        <input type="text" className="search-bar" value={search} onChange={updateSearch}/>
+        <button type="submit" className="search-button">Search button BABY</button>
+      </form>
+      <div className="recipes">
+      {recipes.map(recipe => (
+        <Recipe 
+        key={recipe.recipe.label}
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories} 
+        image={recipe.recipe.image}
+        ingredients= {recipe.recipe.ingredients}
+        />
+      ))}
+      </div>
     </div>
   );
 }
+
 
 export default App;
